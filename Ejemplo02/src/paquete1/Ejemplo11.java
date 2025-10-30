6fbc0f2978c87ed3421e1ef3a862a70b815942b1
TheMostSilly <geovannacastro058@gmail.com>
2025-10-30 10:30:53 -0500

    NuevosCambios

--- Ejemplo02/src/paquete1/Ejemplo11.java
+++ Ejemplo02/src/paquete1/Ejemplo11.java
@@ -18,30 +18,28 @@ public class Ejemplo11 {
         // Ingreso de valores por teclado
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US); // para ingresar los decimales con 
-                                        // punto
-                                        // no con coma
+        // punto
+        // no con coma
         // declarar las variables
         String nombreAsignatura1;
         String nombreAsignatura2;
         double notaAsignatura1;
         double notaAsignatura2;
-        
+
         System.out.println("Ingrese nombre de asignatura 1");
         nombreAsignatura1 = entrada.nextLine(); // nextline me permite recibir una cadena
         System.out.println("Ingrese nota de asignatura 1");
         notaAsignatura1 = entrada.nextDouble();
-        System.out.println("Ingrese nombre de asignatura 2");
-        nombreAsignatura2 = entrada.nextLine();
-        System.out.println("Ingrese nota de asignatura 2");
-        notaAsignatura2 = entrada.nextDouble();
-        
-        System.out.printf("Asignatura 1: %s\nNota 1:%.2f\nAsignatura 2:%s\n"
-                + "Nota 2: %.2f\n",
+
+        System.out.printf("Asignatura 1: %s\nNota 1:%.2f",
                 nombreAsignatura1,
-                notaAsignatura1,
-                nombreAsignatura2,
-                notaAsignatura2);
-   
+                notaAsignatura1
+        );
+
+
+
+
+
     }
-    
+
 }
